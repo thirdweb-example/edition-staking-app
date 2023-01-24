0ece662bb0de51caba9c5fa75603d4738409fce0
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-01-25 02:33:39 +0530

    feat: init (#1)
    * feat: init
    
    * some updates
    
    * update readmeg

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const editionDropContractAddress =
+  "0xB0eC9a37DD05F9DC4050f9E9347979673057d582";
+export const tokenContractAddress =
+  "0x00a09199c02a269f1019A2832b334A660CB20395";
+export const stakingContractAddress =
+  "0x334aDB0825bA13d6f03bfEA44A9C7764b741f589";
---
+++ pages/mint.tsx
@@ -0,0 +1,39 @@
+import { Web3Button } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { editionDropContractAddress } from "../consts/contractAddresses";
+import styles from "../styles/Home.module.css";
+
+const Mint: NextPage = () => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Mint An NFT!</h1>
+
+      <p className={styles.explain}>
+        Here is where we use our <b>Edition Drop</b> contract to allow users to
+        mint one of the NFTs that we lazy minted.
+      </p>
+      <hr className={`${styles.smallDivider} ${styles.detailPageHr}`} />
+
+      <Web3Button
+        colorMode="dark"
+        accentColor="#5204BF"
+        contractAddress={editionDropContractAddress}
+        action={(contract) => contract.erc1155.claim(0, 1)}
+        onSuccess={() => {
+          alert("NFT Claimed!");
+          router.push("/stake");
+        }}
+        onError={(error) => {
+          alert(error);
+        }}
+      >
+        Claim An NFT
+      </Web3Button>
+    </div>
+  );
+};
+
+export default Mint;
